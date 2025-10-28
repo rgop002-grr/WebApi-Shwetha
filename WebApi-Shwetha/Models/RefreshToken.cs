fcc7c211fed739fd01011e86cd3ef76d72c574cd
manas <swetha.nuthula05@gmail.com>
2025-10-28 15:11:47 +0530

    Done with Refresh token

---
+++ WebApi-Shwetha/Models/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi_Shwetha.Controllers
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+        public DateTime? Revoked { get; set; }
+
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public bool IsActive => Revoked == null && !IsExpired;
+
+    }
+}
