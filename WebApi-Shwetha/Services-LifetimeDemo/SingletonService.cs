b19642a235e3b8b15effe73f5742a94520ccb7c3
manas <swetha.nuthula05@gmail.com>
2025-11-20 13:33:41 +0530

    Done with Dependency Injection.

---
+++ WebApi-Shwetha/Services-LifetimeDemo/SingletonService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApi_Shwetha.Services_LifetimeDemo
+{
+    public class SingletonService
+    {
+        private readonly string _guid;
+
+        public SingletonService()
+        {
+            _guid = Guid.NewGuid().ToString();
+        }
+
+        public string GetGuid()
+        {
+            return _guid;
+        }
+    }
+}
+
---
+++ WebApi-Shwetha/Services-LifetimeDemo/TransientService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApi_Shwetha.Services_LifetimeDemo
+{
+    public class TransientService
+    {
+        private readonly string _guid;
+
+        public TransientService()
+        {
+            _guid = Guid.NewGuid().ToString();
+        }
+
+        public string GetGuid()
+        {
+            return _guid;
+        }
+    }
+}
+    
