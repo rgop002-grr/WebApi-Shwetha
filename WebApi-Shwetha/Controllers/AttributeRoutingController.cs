98a07457829eaed80f71a8201e8ef3ed6adf0876
manas <swetha.nuthula05@gmail.com>
2025-12-02 15:52:05 +0530

    Added Attribute Routing controller and Convention Routing controller.

---
+++ WebApi-Shwetha/Controllers/AttributeRoutingController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DataTransferObject;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi_Shwetha.Controllers
+{
+    [Route("Swetha")]
+    [ApiController]
+    public class AttributeRoutingController : ControllerBase
+    {
+        private readonly EmployeeService _employeeService;
+
+        public AttributeRoutingController(EmployeeService employeeService)
+        {
+           _employeeService=employeeService;
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        public IActionResult GetAll()
+        {
+            var employees = _employeeService.GetAllEmployees();
+            return Ok(employees);
+        }
+
+        [HttpGet]
+        [Route("get-by-id/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var employee = _employeeService.GetEmployeeById(id);
+            if (employee == null)
+                return NotFound();
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(EmployeeDto emp)
+        {
+            _employeeService.AddEmployee(emp);
+            return Ok("Employee added successfully!");
+        }
+
+
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            _employeeService.DeleteEmployee(id);
+            return Ok("Employee deleted successfully!");
+        }
+    }
+}
+
+
+        
+   
---
+++ WebApi-Shwetha/Controllers/ConventionRoutingController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Shwetha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConventionRoutingController : ControllerBase
+    {
+        private readonly EmployeeService _employeeService;
+
+        public ConventionRoutingController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+       public IActionResult GetAll()
+        {
+            var employees = _employeeService.GetAllEmployees();
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+       public IActionResult GetById(int id)
+        {
+            var employee = _employeeService.GetEmployeeById(id);
+            if (employee == null)
+                return NotFound();
+            return Ok(employee);
+        }
+    }
+}
+
