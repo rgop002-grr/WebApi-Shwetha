cd7ce93a9b1931c0c8c20184310471c77c2c131c
manas <swetha.nuthula05@gmail.com>
2025-12-10 20:00:09 +0530

    Update on conventional routing.

--- WebApi-Shwetha/Controllers/ConventionRoutingController.cs
+++ WebApi-Shwetha/Controllers/ConventionRoutingController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+//Attribute routing overrides conventional routing, so conventional routing will not work with attribute routing.
+
 namespace WebApi_Shwetha.Controllers
 {
     
